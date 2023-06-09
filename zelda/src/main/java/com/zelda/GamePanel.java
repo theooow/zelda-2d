167af89e57e74d8829be1ae72efab4a80bbc251e
CHOUABBIA Théo-Selim <theo-selim.chouabbia@etu.univ-amu.fr>
2023-06-09 14:56:49 +0200

    Implements basics of game loop and controls.

--- zelda/src/main/java/com/zelda/GamePanel.java
+++ zelda/src/main/java/com/zelda/GamePanel.java
@@ -19,13 +19,23 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreencol; //768 pixels
     final int screenHeight = tileSize * maxScreenrow; //576 pixels
 
+    final int FPS = 60;
+
+    KeyHandler keyHandler = new KeyHandler();
     Thread gameThread;
+
+    //Player settings
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
         super();
         setPreferredSize(new Dimension(screenWidth, screenHeight));
         setBackground(Color.BLACK);
         setDoubleBuffered(true);
+        addKeyListener(keyHandler);
+        setFocusable(true);
     }
 
     public void startGameThread() {
@@ -38,26 +48,44 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run() {
         while(gameThread != null){
-            try {
-                Thread.sleep(1000/60);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            double drawInterval = 1000000000.0 / FPS; // 0.01666666666666666666666666666667 seconds
+            double nextDrawTime = System.nanoTime() + drawInterval;
 
             update();
             repaint();
+
+            try {
+                double remainingTimeToDraw = nextDrawTime - System.nanoTime();
+                if(remainingTimeToDraw < 0)
+                    remainingTimeToDraw = 0;
+
+                Thread.sleep((long) remainingTimeToDraw / 1000000);
+
+                nextDrawTime += drawInterval;
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void update() {
-
+        if(keyHandler.upPressed){
+            playerY -= playerSpeed;
+        } else if(keyHandler.downPressed){
+            playerY += playerSpeed;
+        } else if(keyHandler.leftPressed){
+            playerX -= playerSpeed;
+        } else if(keyHandler.rightPressed){
+            playerX += playerSpeed;
+        }
     }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
 
         g2.setColor(Color.WHITE);
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
         g2.dispose();
     }
 
